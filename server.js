706d4caa95c5fdccaf5d8f1668b1320200a3d36c
marcos.hosoya <marcos.hosoya@certsys.com.br>
2016-10-13 17:40:13 -0300

    add more .js files

--- server.js
+++ server.js
@@ -26,6 +26,94 @@ app.use(passport.initialize());
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
+mongoose.connect(config.database);
+
+// pass passport for configuration
+require('./config/passport')(passport);
+
+// bundle our routes
+var apiRoutes = express.Router();
+
+// connect the api routes under /api/*
+app.use('/api', apiRoutes);
+
+// create a new user account (POST http://localhost:8080/api/signup)
+apiRoutes.post('/signup', function(req, res) {
+    if (!req.body.name || !req.body.password) {
+        res.json({success: false, msg: 'Please pass name and password.'});
+    } else {
+        var newUser = new User({
+            name: req.body.name,
+            password: req.body.password
+        });
+        // save the user
+        newUser.save(function(err) {
+            if (err) {
+                return res.json({success: false, msg: 'Username already exists.'});
+            }
+            res.json({success: true, msg: 'Successful created new user.'});
+        });
+    }
+});
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+            // check if password matches
+            user.comparePassword(req.body.password, function (err, isMatch) {
+                if (isMatch && !err) {
+                    // if user is found and password is right create a token
+                    var token = jwt.encode(user, config.secret);
+                    // return the information including token as JSON
+                    res.json({success: true, token: 'JWT ' + token});
+                } else {
+                    res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+                }
+            });
+        }
+    });
+});
+
+// route to a restricted info (GET http://localhost:8080/api/memberinfo)
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+        var decoded = jwt.decode(token, config.secret);
+        User.findOne({
+            name: decoded.name
+        }, function(err, user) {
+            if (err) throw err;
+
+            if (!user) {
+                return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+            } else {
+                res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+            }
+        });
+    } else {
+        return res.status(403).send({success: false, msg: 'No token provided.'});
+    }
+});
+
+var getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
 
 // Start the server
 app.listen(port);
